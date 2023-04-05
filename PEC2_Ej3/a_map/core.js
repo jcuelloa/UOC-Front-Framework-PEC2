445b3ddcf5f4e9570f9f8b172a8349dc6f9aee69
Juan Antonio Cuello Alarcon <jcuelloa@uoc.edu>
2023-04-05 19:39:13 +0200

    ejer3 a_map completado

--- PEC2_Ej3/a_map/core.js
+++ PEC2_Ej3/a_map/core.js
@@ -1,18 +1,50 @@
+const { kMaxLength } = require("buffer");
+
 function multiplyBy10(array) {
   // your code here
+
+  const array2 = array.map(value => value * 10);
+  return array2;
+
+  // const array2 = array.map(multiplicarPor10);
+  // function multiplicarPor10(value){
+  //   return value * 10;
+  // }
+
 }
 
 function shiftRight(array) {
   // your code here
+  const array2 = array.map((value, i, a) => a[(i + a.length - 1) % a.length]);
+  return array2;
+
+  //Para rotar a la izquierda
+  //const array3 = array.map((value, i, a) => a[(i + 1) % a.length]);
+
+
 }
 
 function onlyVowels(array) {
   // your code here
+  const array2 = array.map((value) => value.replace(/[bcdfghjklmnñpqrstvwxyz]/gi, ''));  
+  return array2;
+  
+
 }
 
 function doubleMatrix(array) {
   // your code here
+  const array2 = array.map((lista)=>{
+    let lista_doble = [];
+    for(let i = 0; i < lista.length; i++){
+        lista_doble.push(lista[i] * 2);
+    }
+    return lista_doble;
+  })
+  return array2;
 }
+
+
 
 module.exports = {
   multiplyBy10,
