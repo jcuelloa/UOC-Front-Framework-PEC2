11b8f3b36b84e52700d8ac3a36d845b162983abb
Juan Antonio Cuello Alarcon <jcuelloa@uoc.edu>
2023-04-07 15:58:35 +0200

    entryCalculator hecha

--- PEC2_Ej3/f_zoo/core.js
+++ PEC2_Ej3/f_zoo/core.js
@@ -1,5 +1,47 @@
+const data = require('./data');
+
 function entryCalculator(entrants) {
   // your code here
+  let precioAdult = data.prices.Adult;
+  let precioSenior = data.prices.Senior;
+  let precioChild = data.prices.Child;
+  
+
+  // //Muestro precios
+  // console.log("** Precios **");
+  // console.log(`Adulto: ${precioAdult}`);
+  // console.log(`Senior ${precioSenior}`);
+  // console.log(`Child ${precioChild}`);
+
+  // //Muestro argumento
+  // console.log(entrants);
+
+  //No argumentos de entrada
+  if (entrants === undefined){
+    return 0;
+  }
+
+  //Objeto vacio
+  else if (Object.entries(entrants).length === 0){
+    return 0;
+  }
+
+  //Hago el recuento de entradas
+  else{
+    let totalAdultos = entrants.Adult;
+    let totalSenior = entrants.Senior;
+    let totalChild = entrants.Child;
+  
+    //console.log(`total Adultos --> ${totalAdultos}`);
+
+    let precioTotal = totalAdultos * precioAdult + totalSenior * precioSenior + totalChild * precioChild;
+    
+    return precioTotal;
+
+    
+  }
+  
+
 }
 
 function schedule(dayName) {
