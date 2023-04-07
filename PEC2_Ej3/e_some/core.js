aef67be77735ded7fd9584bef2e4e921f8bc0a1f
Juan Antonio Cuello Alarcon <jcuelloa@uoc.edu>
2023-04-07 15:06:53 +0200

    e_some hecho

--- PEC2_Ej3/e_some/core.js
+++ PEC2_Ej3/e_some/core.js
@@ -2,28 +2,55 @@
 // array are numbers greater than 10.
 
 function anyGreaterThan10 (input) {
-  return input;
+
+  //Con funcion flecha
+  return input.some(valor => valor > 10);
+
+  // return input.some(mayor10);
+
+  // function mayor10(valor){
+  //   return valor>10; 
+  // }
+
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
 function longWord (input) {
-  return input;
+  return input.some(cadena => cadena.length >10);
+
 };
 
 // Check to see if any of the elements in
 // the matrix are true.
 
 function truePossibilities (input) {
-  return input;
+  
+  return input.some (array => {
+    let algunTrue = false;
+    for (let i=0; i<array.length; i++){
+      if (array[i]==true){
+        algunTrue = true;
+      }
+    }
+    return algunTrue;
+  });
+  
 };
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
 function lostCarcosa (input) {
-  return input;
+  
+  return input.some (frase => /Lost/.test(frase));
+  
+  // return input.some (existePalabraLost);
+  // function existePalabraLost(frase){
+  //   return /Lost/.test(frase);
+  // }
+
 };
 
 module.exports = {
