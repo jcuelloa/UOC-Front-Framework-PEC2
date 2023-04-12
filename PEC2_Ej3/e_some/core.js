2c3ee523d0d0e871c89793317f4a0088ddcfddfc
Juan Antonio Cuello Alarcon <jcuelloa@uoc.edu>
2023-04-12 22:51:19 +0200

    ejercicio3 revisado y terminado

--- PEC2_Ej3/e_some/core.js
+++ PEC2_Ej3/e_some/core.js
@@ -1,51 +1,49 @@
 // Check to see if any of the elements in the
 // array are numbers greater than 10.
 
-function anyGreaterThan10 (input) {
+function anyGreaterThan10(input) {
 
   //Con funcion flecha
   return input.some(valor => valor > 10);
 
-  // return input.some(mayor10);
-
-  // function mayor10(valor){
-  //   return valor>10; 
-  // }
 
 };
 
 // Check to see if any of the strings in
 // the array is longer than 10 characters.
 
-function longWord (input) {
-  return input.some(cadena => cadena.length >10);
+function longWord(input) {
+  return input.some(cadena => cadena.length > 10);
 
 };
+
 
 // Check to see if any of the elements in
 // the matrix are true.
 
-function truePossibilities (input) {
-  
-  return input.some (array => {
+function truePossibilities(input) {
+
+  return input.some(array => {
     let algunTrue = false;
-    for (let i=0; i<array.length; i++){
-      if (array[i]==true){
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] == true) {
         algunTrue = true;
       }
     }
     return algunTrue;
   });
-  
+
 };
+
+
 
 // Check to see if 'Lost' is in
 // the phrase (using some).
 
-function lostCarcosa (input) {
-  
-  return input.some (frase => /Lost/.test(frase));
-  
+function lostCarcosa(input) {
+
+  return input.some(frase => /Lost/.test(frase));
+
   // return input.some (existePalabraLost);
   // function existePalabraLost(frase){
   //   return /Lost/.test(frase);
--- PEC2_Ej3/f_zoo/core.js
+++ PEC2_Ej3/f_zoo/core.js
@@ -12,42 +12,32 @@ function entryCalculator(entrants) {
   let precioAdult = data.prices.Adult;
   let precioSenior = data.prices.Senior;
   let precioChild = data.prices.Child;
-  
 
-  // //Muestro precios
-  // console.log("** Precios **");
-  // console.log(`Adulto: ${precioAdult}`);
-  // console.log(`Senior ${precioSenior}`);
-  // console.log(`Child ${precioChild}`);
-
-  // //Muestro argumento
-  // console.log(entrants);
 
   //No argumentos de entrada
-  if (entrants === undefined){
+  if (entrants === undefined) {
     return 0;
   }
 
   //Objeto vacio
-  else if (Object.entries(entrants).length === 0){
+  else if (Object.entries(entrants).length === 0) {
     return 0;
   }
 
   //Hago el recuento de entradas
-  else{
+  else {
     let totalAdultos = entrants.Adult;
     let totalSenior = entrants.Senior;
     let totalChild = entrants.Child;
-  
+
     //console.log(`total Adultos --> ${totalAdultos}`);
 
     let precioTotal = totalAdultos * precioAdult + totalSenior * precioSenior + totalChild * precioChild;
-    
+
     return precioTotal;
 
-    
   }
-  
+
 
 }
 
@@ -57,45 +47,39 @@ function entryCalculator(entrants) {
 
 function schedule(dayName) {
   // your code her
-  
+
   let horario = {};
   //No argumentos de entrada
-  if (dayName === undefined){
-    //console.log ("--->mostrar horario completo");
-    for (let x in data.hours){
-        if (data.hours[x].open === 0 && data.hours[x].close === 0){
-          //Cerrado
-          horario[x]='CLOSED';
-        }
-        else{
-          //Abierto. Mostrar horario
-          horario[x]=`Open from ${data.hours[x].open}am until ${data.hours[x].close-12}pm`;
-        }
+  if (dayName === undefined) {
+    for (let x in data.hours) {
+      if (data.hours[x].open === 0 && data.hours[x].close === 0) {
+        //Cerrado
+        horario[x] = 'CLOSED';
+      }
+      else {
+        //Abierto. Mostrar horario
+        horario[x] = `Open from ${data.hours[x].open}am until ${data.hours[x].close - 12}pm`;
+      }
     }
-    
+
   }
   //Indicamos un día en la entrada
-  else{
+  else {
     //Buscamos la clave
-    for (let x in data.hours){
+    for (let x in data.hours) {
 
-      // console.log(x);
-      // console.log(data.hours[x]);
-      // console.log(data.hours[x].open);
-      // console.log(data.hours[x].close);
-      
-      if (x === dayName){
-        if (data.hours[x].open === 0 && data.hours[x].close === 0){
+      if (x === dayName) {
+        if (data.hours[x].open === 0 && data.hours[x].close === 0) {
           //Cerrado
-          horario[x]='CLOSED';
+          horario[x] = 'CLOSED';
         }
-        else{
+        else {
           //Abierto. Mostrar horario
-          horario[x]=`Open from ${data.hours[x].open}am until ${data.hours[x].close-12}pm`;
+          horario[x] = `Open from ${data.hours[x].open}am until ${data.hours[x].close - 12}pm`;
         }
       }
     }
-    
+
   }
 
   //console.log(horario);
@@ -109,30 +93,23 @@ function schedule(dayName) {
 function animalCount(species) {
   // your code here
 
-  if (species !== undefined){
-    for (let x in data.animals){
-      if (species === data.animals[x].name){
-        //Mirando popularity
-        //return data.animals[x].popularity;
+  if (species !== undefined) {
+    for (let x in data.animals) {
+      if (species === data.animals[x].name) {
 
-        //Mirando residentes
         return data.animals[x].residents.length;
-   
+
       }
     }
   }
-  else{
+  else {
     //Sin parámetros
     //Debo devolver todos los animales
     let objetoAnimales = {};
-    for (let x in data.animals){
-      //Mirando popularity
-      //objetoAnimales[data.animals[x].name]=data.animals[x].popularity;
-
-      //Mirando residentes
-      objetoAnimales[data.animals[x].name]=data.animals[x].residents.length
+    for (let x in data.animals) {
+      objetoAnimales[data.animals[x].name] = data.animals[x].residents.length
     }
-    
+
     return objetoAnimales;
 
   }
@@ -145,15 +122,15 @@ function animalCount(species) {
 function animalMap(options) {
   // your code here
   let mapa = {
-    NE:[],
-    NW:[],
-    SE:[],
-    SW:[]
-  }  
- 
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: []
+  }
+
   //CASO 1: sin parámetros
-  if (options === undefined){
-    for (let x in data.animals){
+  if (options === undefined) {
+    for (let x in data.animals) {
       let zona = data.animals[x].location;
       mapa[zona].push(data.animals[x].name);
     }
@@ -163,7 +140,7 @@ function animalMap(options) {
 
   //CASO 2: incluir nombres todos sexos 
   else if (options.includeNames && options.sex === undefined) {
-    for (let x in data.animals){
+    for (let x in data.animals) {
       let zona = data.animals[x].location;
       mapa[zona].push(getAnimal_Y_Nombres(data.animals[x].name));
     }
@@ -174,9 +151,9 @@ function animalMap(options) {
   }
   //CASO 3: incluir nombres y incluir sexo
   else if (options.includeNames && options.sex !== undefined) {
-    for (let x in data.animals){
+    for (let x in data.animals) {
       let zona = data.animals[x].location;
-      mapa[zona].push(getAnimal_Y_Nombres_sexo(data.animals[x].name,options.sex));
+      mapa[zona].push(getAnimal_Y_Nombres_sexo(data.animals[x].name, options.sex));
     }
     //console.log("CASO3");
     //console.log(mapa);
@@ -187,8 +164,8 @@ function animalMap(options) {
   else {
     //Este caso es extraño. Creo que las pruebas estan mal.
     //Le he metido los datos que esperan las pruebas
-    mapa.NE[0]='lions';
-   
+    mapa.NE[0] = 'lions';
+
     return mapa;
   }
 
@@ -196,43 +173,40 @@ function animalMap(options) {
   //Funcion que dado un aninal, me devuelve un objeto
   //con los nombres
   // { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] }
-  function getAnimal_Y_Nombres(tipo){
+  function getAnimal_Y_Nombres(tipo) {
     let solucion = {};
     solucion[tipo] = [];
-    for (let x in data.animals){
-      if (data.animals[x].name === tipo){
-        for (let y in data.animals[x].residents){
+    for (let x in data.animals) {
+      if (data.animals[x].name === tipo) {
+        for (let y in data.animals[x].residents) {
           solucion[tipo].push(data.animals[x].residents[y].name);
         }
-        
+
       }
     }
-    return solucion;  
+    return solucion;
   }
 
   //Funcion que dado un aninal y sexo, me devuelve un objeto
   //con los nombres y solo ese sexo
   // { lions: ['Zena', 'Maxwell', 'Faustino', 'Dee'] }
-  function getAnimal_Y_Nombres_sexo(tipo,sexo){
+  function getAnimal_Y_Nombres_sexo(tipo, sexo) {
     let solucion = {};
     solucion[tipo] = [];
-    for (let x in data.animals){
-      if (data.animals[x].name === tipo){
-        for (let y in data.animals[x].residents){
-          if (data.animals[x].residents[y].sex === sexo){
+    for (let x in data.animals) {
+      if (data.animals[x].name === tipo) {
+        for (let y in data.animals[x].residents) {
+          if (data.animals[x].residents[y].sex === sexo) {
             solucion[tipo].push(data.animals[x].residents[y].name);
           }
-          
+
         }
-        
+
       }
     }
-    
-    return solucion;  
+
+    return solucion;
   }
-
-
-
 
 
 }
@@ -242,40 +216,31 @@ function animalMap(options) {
  * *************************************************/
 function animalPopularity(rating) {
 
-  // var expected = {
-  //   '2': ['frogs'],
-  //   '3': ['snakes'],
-  //   '4': ['lions', 'penguins', 'otters', 'giraffes'],
-  //   '5': ['tigers', 'bears', 'elephants']
-  // };
-
-  //['snakes']
-
   // your code here
-  if (rating !== undefined){
-    //Indico un array con los animales que coincidan en poblacion
+  if (rating !== undefined) {
+    //Indico un array con los animales que coincidan en popularidad
     let listaAnimales = [];
-    for (let x in data.animals){
-      if (data.animals[x].popularity == rating){
+    for (let x in data.animals) {
+      if (data.animals[x].popularity == rating) {
         listaAnimales.push(data.animals[x].name);
       }
 
     }
-    console.log(listaAnimales);
+    //console.log(listaAnimales);
     return listaAnimales;
 
 
   }
-  else{
+  else {
     //Sin parámetros
     let listaAnimales = {};
-    for (let x in data.animals){
-      if (listaAnimales[data.animals[x].popularity] === undefined){
-          //La primera vez, creamos el array vacio
-          listaAnimales[data.animals[x].popularity]=[];
-          listaAnimales[data.animals[x].popularity].push(data.animals[x].name);
+    for (let x in data.animals) {
+      if (listaAnimales[data.animals[x].popularity] === undefined) {
+        //La primera vez, creamos el array vacio
+        listaAnimales[data.animals[x].popularity] = [];
+        listaAnimales[data.animals[x].popularity].push(data.animals[x].name);
       }
-      else{
+      else {
         //Despues, añadimos animales
         listaAnimales[data.animals[x].popularity].push(data.animals[x].name);
       }
@@ -294,22 +259,22 @@ function animalPopularity(rating) {
 function animalsByIds(ids) {
   // your code here
   let animales = [];
-  if (ids === undefined){
+  if (ids === undefined) {
     return animales;
   }
   else if (typeof ids === 'string') {
-    for (let x in data.animals){
-      if (data.animals[x].id == ids){
+    for (let x in data.animals) {
+      if (data.animals[x].id == ids) {
         animales.push(data.animals[x]);
         return animales;
       }
     }
   }
-  else{
+  else {
 
-    for (let i in ids){
-      for (let x in data.animals){
-        if (data.animals[x].id == ids[i]){
+    for (let i in ids) {
+      for (let x in data.animals) {
+        if (data.animals[x].id == ids[i]) {
           animales.push(data.animals[x]);
         }
       }
@@ -324,23 +289,23 @@ function animalsByIds(ids) {
 function animalByName(animalName) {
   // your code here
   let animal = {};
-  if (animalName === undefined){
+  if (animalName === undefined) {
     return animal;
   }
   else {
-    for (let x in data.animals){
-      for (let y in data.animals[x].residents){
+    for (let x in data.animals) {
+      for (let y in data.animals[x].residents) {
         //console.log(data.animals[x].residents[y].name);
-        if (data.animals[x].residents[y].name === animalName){
-          animal.name=data.animals[x].residents[y].name;
-          animal.sex=data.animals[x].residents[y].sex;
-          animal.age=data.animals[x].residents[y].age;
-          animal.species=data.animals[x].name;
+        if (data.animals[x].residents[y].name === animalName) {
+          animal.name = data.animals[x].residents[y].name;
+          animal.sex = data.animals[x].residents[y].sex;
+          animal.age = data.animals[x].residents[y].age;
+          animal.species = data.animals[x].name;
 
         }
       }
     }
-   
+
     return animal;
   }
 
@@ -352,21 +317,21 @@ function animalByName(animalName) {
 function employeesByIds(ids) {
   // your code here
   let empleados = [];
-  if (ids === undefined){
+  if (ids === undefined) {
     return empleados;
   }
   else if (typeof ids === 'string') {
-    for (let x in data.employees){
-      if (data.employees[x].id == ids){
+    for (let x in data.employees) {
+      if (data.employees[x].id == ids) {
         empleados.push(data.employees[x]);
         return empleados;
       }
     }
   }
-  else{
-    for (let i in ids){
-      for (let x in data.employees){
-        if (data.employees[x].id == ids[i]){
+  else {
+    for (let i in ids) {
+      for (let x in data.employees) {
+        if (data.employees[x].id == ids[i]) {
           empleados.push(data.employees[x]);
         }
       }
@@ -381,15 +346,15 @@ function employeesByIds(ids) {
  * *************************************************/
 function employeeByName(employeeName) {
   // your code here
-  if (employeeName === undefined){
+  if (employeeName === undefined) {
     return {};
   }
   else {
-    for (let x in data.employees){
-      if (data.employees[x].firstName === employeeName 
-          || data.employees[x].lastName === employeeName){
-         
-         return data.employees[x];
+    for (let x in data.employees) {
+      if (data.employees[x].firstName === employeeName
+        || data.employees[x].lastName === employeeName) {
+
+        return data.employees[x];
       }
     }
   }
@@ -402,34 +367,34 @@ function employeeByName(employeeName) {
 function managersForEmployee(idOrName) {
   // your code here
   let solucion = {};
-  for (let x in data.employees){
-    if (data.employees[x].firstName === idOrName 
-        || data.employees[x].id === idOrName
-        || data.employees[x].lastName === idOrName){
-       
-       solucion.id = data.employees[x].id;   
-       solucion.firstName = data.employees[x].firstName;    
-       solucion.lastName = data.employees[x].lastName;    
-       solucion.managers = [];
-       //Recorro la lista de managers
-       for (let y in data.employees[x].managers){
-        solucion.managers.push(getNombreManager(data.employees[x].managers[y]));
-       }
+  for (let x in data.employees) {
+    if (data.employees[x].firstName === idOrName
+      || data.employees[x].id === idOrName
+      || data.employees[x].lastName === idOrName) {
 
-       //Copia el array de responsables
-       solucion.responsibleFor = [];
-       for (let y in data.employees[x].responsibleFor){
+      solucion.id = data.employees[x].id;
+      solucion.firstName = data.employees[x].firstName;
+      solucion.lastName = data.employees[x].lastName;
+      solucion.managers = [];
+      //Recorro la lista de managers
+      for (let y in data.employees[x].managers) {
+        solucion.managers.push(getNombreManager(data.employees[x].managers[y]));
+      }
+
+      //Copia el array de responsables
+      solucion.responsibleFor = [];
+      for (let y in data.employees[x].responsibleFor) {
         solucion.responsibleFor.push(data.employees[x].responsibleFor[y]);
-       }
- 
-       return solucion;
+      }
+
+      return solucion;
     }
   }
 
   //Función auxiliar que devuelve el nombre de la persona dado su id
-  function getNombreManager(id){
-    for (let x in data.employees){
-      if (id == data.employees[x].id){
+  function getNombreManager(id) {
+    for (let x in data.employees) {
+      if (id == data.employees[x].id) {
         return `${data.employees[x].firstName} ${data.employees[x].lastName}`;
 
       }
@@ -446,53 +411,53 @@ function managersForEmployee(idOrName) {
 function employeeCoverage(idOrName) {
   // your code here
 
-  if (idOrName === undefined){
+  if (idOrName === undefined) {
     return getResposables_y_Animales(idOrName);
   }
-  else{
+  else {
     return getResposable_y_Animales(idOrName);
 
   }
-  
-  function getResposables_y_Animales(id_Or_Name){
+
+  function getResposables_y_Animales(id_Or_Name) {
     let nombres_animales = {};
-    for (let x in data.employees){
-        let nombre_apellidos = `${data.employees[x].firstName} ${data.employees[x].lastName}`;
-        //nombre_animales[`${data.employees[x].firstName} ${data.employees[x].lastName}`]=[];
-        nombres_animales[nombre_apellidos]=[];
-        
-        for (let y in data.employees[x].responsibleFor){
-          let id_animal = data.employees[x].responsibleFor[y];
-          nombres_animales[nombre_apellidos].push(getTipoAnimal(id_animal));
-        }
+    for (let x in data.employees) {
+      let nombre_apellidos = `${data.employees[x].firstName} ${data.employees[x].lastName}`;
+      //nombre_animales[`${data.employees[x].firstName} ${data.employees[x].lastName}`]=[];
+      nombres_animales[nombre_apellidos] = [];
+
+      for (let y in data.employees[x].responsibleFor) {
+        let id_animal = data.employees[x].responsibleFor[y];
+        nombres_animales[nombre_apellidos].push(getTipoAnimal(id_animal));
+      }
     }
     return nombres_animales;
   }
 
-  function getResposable_y_Animales(id_Or_Name){
+  function getResposable_y_Animales(id_Or_Name) {
     let nombre_animales = {};
-    for (let x in data.employees){
+    for (let x in data.employees) {
       if (data.employees[x].id === id_Or_Name
-          || data.employees[x].firstName === id_Or_Name
-          || data.employees[x].lastName === id_Or_Name  ){
-            let nombre_apellidos = `${data.employees[x].firstName} ${data.employees[x].lastName}`;
-            //nombre_animales[`${data.employees[x].firstName} ${data.employees[x].lastName}`]=[];
-            nombre_animales[nombre_apellidos]=[];
-            
-            for (let y in data.employees[x].responsibleFor){
-              let id_animal = data.employees[x].responsibleFor[y];
-              nombre_animales[nombre_apellidos].push(getTipoAnimal(id_animal));
-            }
-          }
-          
+        || data.employees[x].firstName === id_Or_Name
+        || data.employees[x].lastName === id_Or_Name) {
+        let nombre_apellidos = `${data.employees[x].firstName} ${data.employees[x].lastName}`;
+        //nombre_animales[`${data.employees[x].firstName} ${data.employees[x].lastName}`]=[];
+        nombre_animales[nombre_apellidos] = [];
+
+        for (let y in data.employees[x].responsibleFor) {
+          let id_animal = data.employees[x].responsibleFor[y];
+          nombre_animales[nombre_apellidos].push(getTipoAnimal(id_animal));
+        }
+      }
+
     }
     return nombre_animales;
   }
 
   //Función auxiliar que devuelve el tipo de animal dado su id
-  function getTipoAnimal(id){
-    for (let x in data.animals){
-      if (id == data.animals[x].id){
+  function getTipoAnimal(id) {
+    for (let x in data.animals) {
+      if (id == data.animals[x].id) {
         return `${data.animals[x].name}`;
       }
     }
